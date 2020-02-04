d071f6176c3e57d1e144991c59ef21bd0989c551
Hector Jaramillo <43076992+HectorJaramillo@users.noreply.github.com>
2020-02-03 18:23:11 -0800

    Update MCM5numeros.cpp

--- MCM5numeros.cpp
+++ MCM5numeros.cpp
@@ -1,5 +1,5 @@
 // MCM5numeros.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
+// Capitulo 2Ejercicio 19
 
 #include "pch.h"
 #include <iostream>
